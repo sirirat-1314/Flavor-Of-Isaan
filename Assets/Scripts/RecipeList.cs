1627bd0a98e1377e075b5112e5536fee22f3a8eb
sirirat-1314 <sirirat.kan@kkumail.com>
2025-03-22 08:00:52 +0700

    Revert ""trash""
    This reverts commit fd4464cca09315865a56fc226ae4f720c9cec8b8.

--- Assets/Scripts/RecipeList.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-[CreateAssetMenu(menuName ="Data/RecipeList")]
-public class RecipeList : ScriptableObject
-{
-    public List<CraftingRecipe> recipes;
-}
